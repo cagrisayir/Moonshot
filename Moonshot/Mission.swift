ce90834ec2ea1dbd751e3f316bb97c889dd86bb7
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-28 23:44:54 +0300

    generic bundle-decodable added

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,28 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Omer Cagri Sayir on 28.12.2023.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+}
