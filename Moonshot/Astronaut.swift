5aa2dfc72b5cf99a359dfcaed073052dce0a275c
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-28 23:12:35 +0300

    Added files(JSON) added images added extension for bundle to decode JSON

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Omer Cagri Sayir on 28.12.2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
