8201bbd695aad8e5831e0056db589d2ebbf6a0f9
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-28 15:19:16 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Omer Cagri Sayir on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
